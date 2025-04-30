d96df0e5149ec945b2e641d630455a273eab1281
Welkin <zywelkin@gmail.com>
2025-04-29 20:16:21 -1000

    feat: simple hv

--- arceos/payload/skernel2/src/main.rs
+++ arceos/payload/skernel2/src/main.rs
@@ -3,15 +3,30 @@
 
 use core::panic::PanicInfo;
 
+//#[no_mangle]
+//unsafe extern "C" fn _start() -> ! {
+//    core::arch::asm!(
+//        "csrr a1, mhartid",
+//        "ld a0, 64(zero)",
+//        "li a7, 8",
+//        "ecall",
+//        options(noreturn)
+//    )
+//}
 #[no_mangle]
 unsafe extern "C" fn _start() -> ! {
+    let a0 = 0x6688 as usize;
+    let a1 = 0x1234 as usize;
+
     core::arch::asm!(
-        "csrr a1, mhartid",
-        "ld a0, 64(zero)",
-        "li a7, 8",
+        "mv a0, {0}",
+        "mv a1, {1}",
+        "li a7, 8",    // SBI reset
         "ecall",
+        in(reg) a0,
+        in(reg) a1,
         options(noreturn)
-    )
+    );
 }
 
 #[panic_handler]
